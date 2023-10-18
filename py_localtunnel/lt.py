a73b9d6643c2cc489135bf637323d71297396c38
TimothyAlexisVass <55708319+TimothyAlexisVass@users.noreply.github.com>
2023-10-18 16:39:20 +0200

    newline at end

--- py_localtunnel/lt.py
+++ py_localtunnel/lt.py
@@ -5,12 +5,12 @@ from py_localtunnel.tunnel import Tunnel
 def run_localtunnel(port: int, subdomain: str):
     t = Tunnel()
     url = t.get_url(subdomain)
-    sys.stdout.write(f"Your url is: {url}")
+    sys.stdout.write(f"Your url is: {url}\n")
     sys.stdout.flush()
     try:
         t.create_tunnel(port)
     except KeyboardInterrupt:
-        sys.stdout.write("\nKeyboardInterrupt: Stopping tunnel...")
+        sys.stdout.write("\nKeyboardInterrupt: Stopping tunnel...\n")
         sys.stdout.flush()
 
     t.stop_tunnel()
